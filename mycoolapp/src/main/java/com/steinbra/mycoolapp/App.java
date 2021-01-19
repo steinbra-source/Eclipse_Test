33316de74914675b203fb3fce9646b5891f47691
steinbra <steinbra@msgn12523.int.root.msg.ag>
2021-01-19 12:15:21 +0100

    Commit using Token

--- mycoolapp/src/main/java/com/steinbra/mycoolapp/App.java
+++ mycoolapp/src/main/java/com/steinbra/mycoolapp/App.java
@@ -8,7 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World of Maven!!!" );
-        System.out.println( "This project is now in my GitHub repository!" );
+        System.out.println("Hello World of Maven!!!");
+        System.out.println("This project is now in my GitHub repository!");
+        System.out.println("Now using Token for GitHub");
     }
 }
