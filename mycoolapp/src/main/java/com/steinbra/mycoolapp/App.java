27945891c8183603d520f4b20fbef9cf201771b1
steinbra <steinbra@msgn12523.int.root.msg.ag>
2021-01-19 12:36:50 +0100

    push again

--- mycoolapp/src/main/java/com/steinbra/mycoolapp/App.java
+++ mycoolapp/src/main/java/com/steinbra/mycoolapp/App.java
@@ -12,5 +12,6 @@ public class App
         System.out.println("This project is now in my GitHub repository!");
         System.out.println("Now using Token for GitHub");
         System.out.println("New version has a readme file now!!!");
+        System.out.println("Let's test the push again!");
     }
 }
