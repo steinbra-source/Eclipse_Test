9fb98b5d26f7473d9c0880f2171362ae1f0dbb96
steinbra <steinbra@msgn12523.int.root.msg.ag>
2021-01-19 11:05:58 +0100

    Changed code of App.java

--- mycoolapp/src/main/java/com/steinbra/mycoolapp/App.java
+++ mycoolapp/src/main/java/com/steinbra/mycoolapp/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World of Maven!!!" );
+        System.out.println( "This project is now in my GitHub repository!" );
     }
 }
